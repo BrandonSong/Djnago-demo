16e621be0d01dd37119d80c740c423e51e1fce3f
BrandonSong <lqisong@163.com>
2019-02-25 17:54:27 +0800

    section5-7

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -9,7 +9,8 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     def __str__(self):
         return self.question_text
@@ -22,3 +23,19 @@ class Chiocce(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+
+# 测试代码
+# class Manufacturer(models.Model):
+#     pass
+#
+# 
+# class Car(models.Model):
+#     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)  # 多对一的外键关系
+
+class Topping(models.Model):
+    pass
+
+
+class Pizza(models.Model):
+    toppings = models.ManyToManyField(Topping)
